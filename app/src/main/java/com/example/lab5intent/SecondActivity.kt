b2c0daf3e8e79bb962710cc193af5ef42959bf70
thanatpump <53934985+thanatpump@users.noreply.github.com>
2019-08-23 16:06:07 +0700

    lab5

---
+++ app/src/main/java/com/example/lab5intent/SecondActivity.kt
@@ -0,0 +1,26 @@
+package com.example.lab5intent
+
+import android.annotation.SuppressLint
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        var data = intent.extras
+        var newStudent = data?.getParcelable<Student>("stdData")
+        idText.text = "Student ID : ${newStudent?.id}"
+        nameText.text = "Student Name : ${newStudent?.name}"
+        ageText.text = "Student Age : ${newStudent?.age}"
+    }
+    fun onClickClose(view: View){
+        finish()
+    }
+}
